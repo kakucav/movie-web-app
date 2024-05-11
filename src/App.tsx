cd17944fa85507a0d10b5cc74a3af2595489e830
kakucav <kakucav@outlook.com>
2024-05-11 20:41:38 +0200

    added app container and bg color

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Router from "./pages/Router";
 
+import styles from "./App.module.scss";
+
 function App(): JSX.Element {
   return (
-    <div>
+    <div className={styles.app_container}>
       <Router />
     </div>
   );
